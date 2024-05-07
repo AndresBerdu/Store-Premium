0f416e121388717dd20fa18a490fb0b117199407
AndresBerdu <andres700crac@gmail.com>
2024-05-07 17:40:07 -0500

    modifed HomeScreen and addd new component NavBarComponent and add styles

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -5,39 +5,41 @@ import { Link } from 'react-router-dom';
 import { PiShoppingCartSimple } from "react-icons/pi";
 import { HiMiniUserCircle } from "react-icons/hi2";
 import { PiShoppingBagThin } from "react-icons/pi";
+import { FiMenu } from "react-icons/fi";
 
 //style
 import '../scss/components/NavBarComponentStyle.scss';
 
 const NavBarComponent = () => {
   return (
-<div className='topbar'>
-        <div className='topbar-logo'>
-          <PiShoppingBagThin className='topbar-logo-icon'/>
-          <h1 className='topbar-logo-text'>PREMIUM STORE</h1>
-        </div>
-        <ul className='topbar-tag'>
-            <li className='topbar-tag-list'>
-                <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Dishes'>Dishes</Link>
-            </li>
-            <li className='topbar-tag-list'>
-                <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Drinks'>Drinks</Link>
-            </li>
-            <li className='topbar-tag-list'>
-                <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Desserts'>Desserts</Link>
-            </li>
-            <li className='topbar-tag-list'>
-                <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Soups'>Soups</Link>
-            </li>
-            <li className='topbar-tag-list'>
-                <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/About Us'>About Us</Link>
-            </li>
-        </ul>
-        <div>
-          <PiShoppingCartSimple className='topbar-icon'/>
-          <HiMiniUserCircle className='topbar-icon'/>
-        </div>
+    <div className='topbar'>
+      <div className='topbar-logo'>
+        <PiShoppingBagThin className='topbar-logo-icon'/>
+        <h1 className='topbar-logo-text'>PREMIUM STORE</h1>
       </div>
+        <ul className='topbar-tag'>
+          <li className='topbar-tag-list'>
+              <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Dishes'>Dishes</Link>
+          </li>
+          <li className='topbar-tag-list'>
+              <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Drinks'>Drinks</Link>
+          </li>
+          <li className='topbar-tag-list'>
+              <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Desserts'>Desserts</Link>
+          </li>
+          <li className='topbar-tag-list'>
+              <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/Soups'>Soups</Link>
+          </li>
+          <li className='topbar-tag-list'>
+              <Link style={{ textDecoration: 'none', color: 'inherit'}} to='/About Us'>About Us</Link>
+          </li>
+        </ul>
+      <div>
+        <PiShoppingCartSimple className='topbar-icon'/>
+        <HiMiniUserCircle className='topbar-icon'/>
+        <FiMenu className='topbar-icon'/>
+      </div>
+    </div>
   )
 }
 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -7,6 +7,13 @@ import { NavBarComponent } from '../components/NavBarComponent';
 //Images
 import dishe1 from '../assets/Dishe1.png';
 import dishe2 from '../assets/Dishe2.png';
+import disheVegetarian1 from '../assets/DisheVegetarian1.jpg';
+import disheVegetarian2 from '../assets/DisheVegetarian2.jpg';
+import disheVegetarian3 from '../assets/DisheVegetarian3.jpg';
+import disheVegetarian4 from '../assets/DisheVegetarian4.jpg';
+
+//icons
+import { LuSalad } from "react-icons/lu";
 
 //Style
 import '../scss/screens/HomeScreenStyle.scss';
@@ -21,16 +28,48 @@ const HomeScreen = () => {
           <p className='body-container-text'>Buy delicius food and change your paladar with much flavors</p>
           <p className='body-container-text-sentence'>Eat is not only eat, is lifes</p>
           <hr className='body-container-line'/>
-          <Link className='body-container-button' style={{ textDecoration: 'none', color: 'inherit'}} to='/Dishes'>DISHES</Link>
+          <div className='body-container-button'>
+            <Link 
+              className='body-container-button-text' 
+              style={{ textDecoration: 'none', color: 'inherit'}}
+              to='/Dishes'
+            >
+              DISHES
+            </Link>
+          </div>
         </div>
-        <div>
-          <img className='body-img' src={dishe1} alt="Dishe with chiken"/>
-          <img className='body-img' src={dishe2} alt="Dishe with chiken" />
+        <div className='body-images'>
+          <img className='body-images-img1' src={dishe1} alt="Dishe with chiken"/>
+          <img className='body-images-img2' src={dishe2} alt="Dishe with chiken" />
         </div>
       </div>
-      <footer>
 
-      </footer>
+      <div className='body2'>
+        <div className='body2-imagesVegetarian'>
+          <img className='body2-imagesVegetarian-img1' src={disheVegetarian1} alt="Dishe Vegetarian chiken"/>
+          <img className='body2-imagesVegetarian-img2' src={disheVegetarian2} alt="Dishe Vegetarian chiken" />
+          <img className='body2-imagesVegetarian-img3' src={disheVegetarian3} alt="Dishe Vegetarian chiken"/>
+          <img className='body2-imagesVegetarian-img4' src={disheVegetarian4} alt="Dishe Vegetarian chiken" />
+        </div>
+        <div className='body2-container2'>
+          <h1 className='body2-container2-title'>FOR ALL THOSE PEOPLE</h1>
+          <p className='body2-container2-text'>You need espcial food, for a espcial person, buy our dishes vegetarians</p>
+          <div className='body2-container2-button'>
+            <Link 
+              className='body2-container2-button-text' 
+              style={{ textDecoration: 'none', color: 'inherit'}}
+              to='/Dishes'
+            >
+              Vegetarian
+              <LuSalad/>
+            </Link> 
+          </div>
+        </div>
+      </div>
+
+      <div>
+        
+      </div>
     </div>
   )
 }
